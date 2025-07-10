ec00c45a8453f63f72804841e468550f295e7f4c
Athankavil <anand.athankavil@sap.com>
2025-07-10 14:15:08 +0530

    Clean build-fixed links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,11 +2,35 @@ module.exports = {
   title: 'Tech Writer Portfolio',
   url: 'https://anandnr47.github.io',
   baseUrl: '/tech-writer-47-docs-as-code/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'anandnr47',
   projectName: 'tech-writer-47-docs-as-code',
+
+  themeConfig: {
+    navbar: {
+      title: 'Tech Writer Portfolio',
+      items: [
+        {
+          to: '/intro',
+          label: 'Home',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/anandnr47/tech-writer-47-docs-as-code',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Anand.`,
+    },
+  },
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
